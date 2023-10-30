e3454960ebc4c40acf9a211e5e8a54ad3f2f5b9a
inna <i.aulova1981@mail.ru>
2023-10-30 17:20:08 +0300

    work4

---
+++ final/Program.cs
@@ -0,0 +1,57 @@
+﻿
+string[] CreateArray(string[] a) {
+    for (int i = 0; i < a.Length; i++) {
+        Console.WriteLine("Введите "+i.ToString()+" элемент массива:");
+        a[i] = Console.ReadLine().ToString();
+    }
+    return a;
+}
+string[] NewMass(string[] a) { 
+    int size=GetCountElem(a);
+    string[] newMass = new string[size];
+    int k = 0;
+    for (int i = 0; i < a.Length; i++) {
+        if ((a[i].Length >= 1) && (a[i].Length <= 3))
+        {
+            newMass[k] = a[i];
+            k++;
+        }
+    }
+    return newMass;
+}
+void PrintNewArray(string []newmass) {
+    Console.WriteLine("Новый массив:");
+    if (newmass.Length == 0)
+    {
+        Console.WriteLine("[]");
+    }
+    else {
+        for (int i = 0; i < newmass.Length; i++) {
+            Console.WriteLine(newmass[i]);
+        }
+    }
+}
+int GetCountElem(string[] a) {
+    int k= 0;
+    for (int i=0;i<a.Length;i++) {
+        if ((a[i].Length >= 1)&&(a[i].Length <= 3)){
+            k += 1;
+        }
+    }
+    return k;
+}
+void PrintMass(string[] a) {
+    Console.WriteLine("Ваш массив:");
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.WriteLine(a[i]);
+    }
+}
+
+Console.WriteLine("Введите количество элементов:");
+int k = Int32.Parse(Console.ReadLine());
+string []mass = new string[k];
+mass=CreateArray(mass);
+PrintMass(mass);
+string[]newmass = NewMass(mass);
+PrintNewArray(newmass);
